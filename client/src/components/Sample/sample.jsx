bef978b8199253e7463da7730f925bb585ebf9ea
Himosh <121594264+Himosh@users.noreply.github.com>
2024-06-21 09:46:27 +0530

    Created folder structure

---
+++ client/src/components/Sample/sample.jsx
@@ -0,0 +1,1 @@
+// this is a jsx file of sample component
---
+++ client/src/data/ongoingProjects.js
@@ -0,0 +1,18 @@
+const ongoingProjects = [
+    {
+        name : "sample project one",
+        duration : "03 months",
+        location : "trincomalee"
+    },
+    {
+        name : "sample project two",
+        duration : "03 months",
+        location : "trincomalee"
+    },
+    {
+        name : "sample project three",
+        duration : "03 months",
+        location : "trincomalee"
+    }
+]
+export default ongoingProjects;
