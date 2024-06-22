812bf2faadb30f901daea3e8b98d17c886faa0f4
ak-the-great <makshayan@std.appsc.sab.ac.lk>
2024-06-22 17:23:32 +0530

    Started to build hero-section in landingPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
-import { Home } from '../src/pages/LandingPage/landingPage';
-import { About } from '../src/pages/AboutPage/aboutPage';
-import { Career } from '../src/pages/CareerPage/careerPage';
-import { Contact } from '../src/pages/ContactPage/contactPage';
+import { landingPage } from './pages/LandingPage/landingPage';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <navBar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/career" element={<Career />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-
-      </div>
-
-    </Router>
+    <div>
+      {landingPage()}
+    </div>
   );
 }
 
