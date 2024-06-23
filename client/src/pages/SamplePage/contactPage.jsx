2c67491289e4302e33a431343fddb3636c15afee
Nilakshan Raveendran <nilaksh2001@gmail.com>
2024-06-23 20:14:41 +0530

    Add contact page

---
+++ client/src/pages/SamplePage/contactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './contactPage.css'
+
+const contactPage = () => {
+  return (
+    <div className="contactPage">
+        <h2>Get in Touch with Us</h2>
+    </div>
+  )
+}
+
+export default contactPage;
