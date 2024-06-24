87fdb5561d5a142a832a35f3bc1893271d9fd67b
Nilakshan Raveendran <nilaksh2001@gmail.com>
2024-06-24 21:15:23 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
 import Contact from './pages/ContactPage/Contact';
 
 function App() {
   return (
     <div>
-      <Navbar />
       <Contact />
-      <Footer />
     </div>
   );
 }
