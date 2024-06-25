99e119856422f86f6e5e597291df21d467e3d0cf
ak-the-great <makshayan@std.appsc.sab.ac.lk>
2024-06-25 18:04:02 +0530

    Career Page has been completed and Contact Page has been updated.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import { LandingPage } from './pages/LandingPage/landingPage';
+import LandingPage from './pages/LandingPage/landingPage';
+import ContactPage from './pages/ContactPage/contactPage';
+import CareerPage from './pages/CareerPage/careerPage';
 
 function App() {
   return (
     <div>
-      {LandingPage()}
+
+      <CareerPage />
+
+
     </div>
   );
 }
