d77c56419c2a6604785de74ab988e8f4512fdfdd
blockdeveloper25 <sujairblockdev@gmail.com>
2024-06-28 21:27:30 +0530

    Adding Staff Datas

---
+++ client/src/data/staffData.js
@@ -0,0 +1,62 @@
+// src/data/teamData.js
+import sujair from "./teamMembers/sujair.png"
+
+const teamData = [
+    {
+      name: "Bertin Jude Niloj",
+      title: "Managing Director",
+      description: "Striving for excellence in every build. Let's elevate together!",
+      image: sujair,
+    },
+    {
+      name: "Bertin Jude Niloj",
+      title: "Managing Director",
+      description: "B.Sc. Hons Civil Engineering, University of Jaffna.",
+      image: sujair,
+    },
+    {
+      name: "Bertin Jude Niloj",
+      title: "Managing Director",
+      description: "B.Sc. Hons Civil Engineering, University of Jaffna. MSc Structural Engineering, University of Pennsylvania.",
+      image: sujair,
+    },
+    {
+      name: "Bertin Jude Niloj",
+      title: "Managing Director",
+      description: "B.Sc. Hons Architecture, University of Moratuwa.",
+      image: "path/to/image4.jpg",
+    },
+    {
+      name: "Bertin Jude Niloj",
+      title: "Managing Director",
+      description: "B.Sc. Hons Civil Engineering, University of Jaffna.",
+      image: "path/to/image5.jpg",
+    },
+    {
+      name: "Bertin Jude Niloj",
+      title: "Managing Director",
+      description: "B.Sc. Hons Civil Engineering, University of Jaffna.",
+      image: "path/to/image6.jpg",
+    },
+    {
+      name: "Bertin Jude Niloj",
+      title: "Managing Director",
+      description: "B.Sc. Hons Civil Engineering, University of Jaffna. MSc Structural Engineering, University of Pennsylvania.",
+      image: "path/to/image7.jpg",
+    },
+    {
+      name: "Bertin Jude Niloj",
+      title: "Managing Director",
+      description: "B.Sc. Hons Architecture, University of Moratuwa.",
+      image: "path/to/image8.jpg",
+    },
+    {
+      name: "Bertin Jude Niloj",
+      title: "Managing Director",
+      description: "B.Sc. Hons Civil Engineering, University of Jaffna.",
+      image: "path/to/image9.jpg",
+    },
+  ];
+  
+  export default teamData;
+  
